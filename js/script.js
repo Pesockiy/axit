2e482e9b4b5d1b73fa0031249b05d909ea0567b3
pesockiy <loremipsum.pt@gmail.com>
2020-06-16 19:23:33 +0300

    made tabs clickable

--- js/script.js
+++ js/script.js
@@ -8,12 +8,11 @@ menuButton.addEventListener('click', function () {
 	if (!this.classList.contains('menu-hamburger--open')) {
 
 		this.classList.add('menu-hamburger--open');
-	body.classList.add('overflowYHidden');
-	navWrap.classList.add('nav-wrap--active');
-
+		body.classList.add('overflowYHidden');
+		navWrap.classList.add('nav-wrap--active');
 
 	} else {
-	this.classList.remove('menu-hamburger--open');
+		this.classList.remove('menu-hamburger--open');
 		body.classList.remove('overflowYHidden');
 		navWrap.classList.remove('nav-wrap--active');
 	}
@@ -37,13 +36,10 @@ let animatedHeader = (function () {
 	}
 
 	function scrollY() {
-
 		return window.pageYOffset || docElem.scrollTop;
-
 	}
 
 	function scrollPage() {
-
 		var sY = scrollY();
 
 		if (sY >= valueToChange) {
@@ -51,10 +47,25 @@ let animatedHeader = (function () {
 		} else {
 			header.classList.remove('header--shrink');
 		}
-
 		scrolling = false;
 	}
 
-
 	init();
 })();
+
+
+const tabs = document.querySelectorAll(".tab__link");
+const tabContent = document.querySelectorAll(".tab__content");
+
+for (let el of tabs) {
+  el.addEventListener("click", function(e) {
+
+	document.querySelector(".tab__link--active").classList.remove("tab__link--active");
+	document.querySelector(".tab__content--active").classList.remove("tab__content--active");
+
+	const index = Array.from(tabs).indexOf(el);
+	const panel = [...tabContent].filter(el => el.getAttribute("data-index") == index);
+	tabs[index].classList.add('tab__link--active')
+	panel[0].classList.add("tab__content--active");
+	});
+ };
