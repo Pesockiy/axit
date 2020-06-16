48ab0e9925310627e1b33a5626f6b405d3c66747
pesockiy <loremipsum.pt@gmail.com>
2020-06-17 00:20:36 +0300

    git commit -m

--- js/script.js
+++ js/script.js
@@ -54,18 +54,78 @@ let animatedHeader = (function () {
 })();
 
 
-const tabs = document.querySelectorAll(".tab__link");
-const tabContent = document.querySelectorAll(".tab__content");
+const tabs = document.querySelectorAll(".tab__link"),
+	tabContent = document.querySelectorAll(".tab__content");
 
 for (let el of tabs) {
-  el.addEventListener("click", function(e) {
 
-	document.querySelector(".tab__link--active").classList.remove("tab__link--active");
-	document.querySelector(".tab__content--active").classList.remove("tab__content--active");
+	el.addEventListener("click", function (e) {
 
-	const index = Array.from(tabs).indexOf(el);
-	const panel = [...tabContent].filter(el => el.getAttribute("data-index") == index);
-	tabs[index].classList.add('tab__link--active')
-	panel[0].classList.add("tab__content--active");
+		document.querySelector(".tab__link--active").classList.remove("tab__link--active");
+		document.querySelector(".tab__content--active").classList.remove("tab__content--active");
+
+		const index = Array.from(tabs).indexOf(el);
+		const panel = [...tabContent].filter(function (el) {
+			return el.getAttribute("data-index") == index;
+		});
+
+		tabs[index].classList.add('tab__link--active')
+		panel[0].classList.add("tab__content--active");
+
 	});
- };
+};
+
+
+
+const links = document.querySelectorAll('.nav__link');
+
+for (let link of links) {
+
+	link.addEventListener('click', function (e) {
+
+		e.preventDefault();
+		const sectionId = link.getAttribute('href');
+		const sectionToScroll = document.querySelector(sectionId);
+
+		if (navWrap.classList.contains('nav-wrap--active')) {
+			navWrap.classList.remove('nav-wrap--active');
+			menuButton.classList.remove('menu-hamburger--open');
+		};
+		smoothScrollTo(sectionToScroll.offsetTop, 500);
+
+	});
+};
+
+
+smoothScrollTo = (function () {
+	let timer,
+		start,
+		factor;
+
+	return function (target, duration) {
+		let offset = window.pageYOffset,
+			delta = target - window.pageYOffset;
+
+		duration = duration || 1000;
+		start = Date.now();
+		factor = 0;
+
+		if (timer) {
+			clearInterval(timer);
+		}
+
+		function step() {
+			let y;
+			factor = (Date.now() - start) / duration;
+			if (factor >= 1) {
+				clearInterval(timer);
+				factor = 1;
+			};
+			y = factor * delta + offset;
+			window.scrollBy(0, y - window.pageYOffset);
+		}
+
+		timer = setInterval(step, 10);
+		return timer;
+	};
+}());
